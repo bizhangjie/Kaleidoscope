17c3674363e5a98c42ac514ab2b926829d11c50e
v000666 <3190103335@zju.edu.cn>
2022-12-20 00:07:49 +0800

    latest -WYJ

--- Apushgithub.py
+++ Apushgithub.py
@@ -14,6 +14,10 @@ def main():
 if __name__ == '__main__':
     # 持续推送到github
     while True:
+        f=MyUtils.txt(MyUtils.projectpath('txt.txt'))
+        f.add(MyUtils.nowstr())
+        MyUtils.CMD(f'cd d:;cd {MyUtils.projectpath()};git add .',silent=True)
         MyUtils.CMD(f'cd d:;cd {MyUtils.projectpath()};git push',silent=True)
+        MyUtils.log('已推送。')
         time.sleep(60*5)
 
--- Cruiser.py
+++ Cruiser.py
@@ -1,11 +1,10 @@
+import multiprocessing
 import time
 
 import A20min
-import Anote
-import MyUtils
-import multiprocessing
-import RAM
 import Apushgithub
+import MyUtils
+import RAM
 
 # 在注意各优先级不冲突的同时，进行巡航
 if __name__ == '__main__':
@@ -15,17 +14,21 @@ if __name__ == '__main__':
     pool1.apply_async(Apushgithub.main)
     pool1.apply_async(RAM.init)
 
-    work=False
-    if work==True:
+    work = False
+    if work == True:
         import DouyinDetect
         import DouyinDownload
+        import provisional
+
+        for i in range(1):
+            pool1.apply_async(provisional.main, ())
+            time.sleep(3)
         for i in range(2):
-            pool1.apply_async(DouyinDetect.main,())
+            pool1.apply_async(DouyinDetect.main, ())
             time.sleep(3)
         for i in range(3):
-            pool1.apply_async(DouyinDownload.main,())
+            pool1.apply_async(DouyinDownload.main, ())
             time.sleep(3)
-
 
     time.sleep(5)
     pool1.close()
