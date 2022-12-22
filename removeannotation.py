3dfae162ff3b8c3cfb1e8d44a516779cb328fffd
v000666 <3190103335@zju.edu.cn>
2022-12-22 16:57:10 +0800

    latest -WYJ

--- removeannotation.py
+++ removeannotation.py
@@ -1,14 +1,19 @@
 import MyUtils
 
-
+def main():
+    for fname in ['Geometry.cpp','Geometry.h','Rtree.cpp','Rtree.h','Rtree.cpp','test.cpp','hw6.cpp','QuadTree.cpp','QuadTree.h',]:
+        path=MyUtils.desktoppath(f'地理空间数据库/hw6-2022 王勇健/{fname}')
+        if not MyUtils.isfile(path):
+            return
+        f=MyUtils.txt(path)
+        newl=[]
+        for i in f.l:
+            if '//'in i and not './/'in i:
+                i=MyUtils.removetail(i,'//')
+            newl.append(i)
+            print(i)
+        f.l=newl
+        f.save()
 
 if __name__ == '__main__':
-    path=MyUtils.desktoppath('地理空间数据库/hw6-2022 王勇健/Geometry.cpp')
-    f=MyUtils.txt(path)
-    newl=[]
-    for i in f.l:
-        if '//'in i and not './/'in i:
-            i=MyUtils.removetail(i,'//')
-        newl.append(i)
-    f.l=newl
-    f.save()
+    main()
