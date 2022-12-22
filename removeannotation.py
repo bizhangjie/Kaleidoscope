7b8f3e5bd7b448f323b31e493fe4aaa4f7d17b52
v000666 <3190103335@zju.edu.cn>
2022-12-22 17:17:38 +0800

    latest -WYJ

--- removeannotation.py
+++ removeannotation.py
@@ -4,6 +4,7 @@ def main():
     for fname in ['Geometry.cpp','Geometry.h','Rtree.cpp','Rtree.h','Rtree.cpp','test.cpp','hw6.cpp','QuadTree.cpp','QuadTree.h',]:
         path=MyUtils.desktoppath(f'地理空间数据库/hw6-2022 王勇健/{fname}')
         if not MyUtils.isfile(path):
+            print('error')
             return
         f=MyUtils.txt(path)
         newl=[]
