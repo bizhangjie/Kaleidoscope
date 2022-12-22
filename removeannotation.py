c100b293c5a0449f787908b522f9f3b592e9e7b8
v000666 <3190103335@zju.edu.cn>
2022-12-22 16:52:03 +0800

    latest -WYJ

--- removeannotation.py
+++ removeannotation.py
@@ -3,11 +3,11 @@ import MyUtils
 
 
 if __name__ == '__main__':
-    path=MyUtils.desktoppath('地理空间数据库/hw6-2022 王勇健/Rtree.cpp')
+    path=MyUtils.desktoppath('地理空间数据库/hw6-2022 王勇健/Geometry.cpp')
     f=MyUtils.txt(path)
     newl=[]
     for i in f.l:
-        if '//'in i:
+        if '//'in i and not './/'in i:
             i=MyUtils.removetail(i,'//')
         newl.append(i)
     f.l=newl
