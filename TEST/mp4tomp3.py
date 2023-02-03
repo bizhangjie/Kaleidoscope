3328f6253017ab0a9a5282308ca3bc04028619a7
v000666 <3190103335@zju.edu.cn>
2023-02-03 23:12:22 +0800

    latest -WYJ

--- TEST/mp4tomp3.py
+++ TEST/mp4tomp3.py
@@ -1,6 +1,6 @@
 import MyUtils
 
 if __name__ == '__main__':
-    tar1=r'D:\standardizedPF\repositories\视频库\长虹剑主，还记得孤王吗\01魔道出-泽田小言-BV1ri4y1A7Fk-KlTUKDpTEqyWl7c4\01魔道出-泽田小言-BV1ri4y1A7Fk-KlTUKDpTEqyWl7c4.mp4'
+    tar1=r'D:\standardizedPF\repositories\皇帝剪辑\音频碎片\无配乐有需要自提-Kt-a-BV19A411u7ne-eUR5t7UDmpxgUOvn\无配乐有需要自提-Kt-a-BV19A411u7ne-eUR5t7UDmpxgUOvn.mp4'
     # src=(r'C:\Users\17371\Videos\cache\coverDistanceVLOGMemories-值鹭羽-BV1be4y1k7wy-ETZbbNJwrk3JconO\sample.mp3')
     MyUtils.mp4tomp3(tar1)
