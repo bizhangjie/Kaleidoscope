967a5a238e44a1636f300614137a7d656588b76a
v000666 <3190103335@zju.edu.cn>
2023-02-03 16:05:49 +0800

    latest -WYJ

--- TwitterDownload.py
+++ TwitterDownload.py
@@ -1,82 +1,92 @@
-import os.path
-import re
-import time
-
-import pyperclip
-import pyautogui
-import requests
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-
 import MyUtils
+import TwitterUtils
 
-TwitterSum = 0
-TotalExceptionNum = 0
-headers = MyUtils.headers
+# region
+allusers = TwitterUtils.allusers
 
-LocalUserSpectrum = MyUtils.file('r', './twitter/AllUsers.txt')
-LocalContentSpectrum = MyUtils.file('r', './twitter/ContentSpectrum.txt')
-LocalPictureSpectrum = MyUtils.file('r', './twitter/PictureSpectrum.txt')
 
-page = MyUtils.chrome()
-# 遍历用户序列
-for UserUID in LocalUserSpectrum:
-    UserUID = LocalUserSpectrum.pop(0)
-    LocalUserSpectrum.append(UserUID)
-    UserUID = MyUtils.MyName(UserUID[0:len(UserUID) - 1])
-    page.get('https://www.twitter.com/' + UserUID)
-    page.set_window_size(400, 1000)
-    time.sleep(3)
-    UserID = MyUtils.title([page])[MyUtils.title([page]).find(')') + 2:MyUtils.title([page]).rfind(UserUID) - 3]
-    ScrollTop = -1
+# endregion
+def getlinklist(page):
+    page = page[0]
+    page.get('https://weibo.com/u/page/follow/5849475471/followGroup?tabid=4864853400880908')
+    MyUtils.sleep(1)
+    return page.elements('//*[@id="scroller"]//div[@class="vue-recycle-scroller__item-view"]//a[contains(@href,"/")]/@href')
 
-    # 保存封面
-    MyUtils.file('wb', f'./twitter/{UserUID}/cover/{UserID}\
-    {time.localtime().tm_year}-{time.localtime().tm_mon}-{time.localtime().tm_mday}.png', \
-                 MyUtils.Element([page, By.XPATH, \
-                                  '/html/body/div[1]/div/div/div[2]/main/div/div/div/div[1]/div/div[2]/div/div/div']).screenshot_as_png)
 
-    # 不断下滚
-    height = -1
-    while MyUtils.getscrolltop([page]) > height:
-        height = MyUtils.getscrolltop([page])
-        time.sleep(2)
-        MyUtils.setscrolltop([page, height])
-        WebContentSpectrum = page.find_elements(By.XPATH, '//div[starts-with(@style,"transform:")]')
-        # 遍历所有tweet
-        for content in WebContentSpectrum:
-            # 滚动到每个tweet，0编号保存截图，找不到则进入下一个WebContentSpectrum
-            if not WebContentSpectrum[0] == page.find_elements(By.XPATH, '//div[starts-with(@style,"transform:")]')[0]:
-                break
-            if MyUtils.getscrolltop([page]) >= content.csv_f['y'] - 53:
+# 更新头像、描述、用户名等信息，并且创建保存用户内容的文件夹
+def adduser(page):
+    page = page[0]
+    author = MyUtils.removetail(page.title(), ' 的个人主页')[1:]
+    useruid = MyUtils.gettail(page.url(), '/')
+    allusers.adduser(useruid, author)
+
+    for i in MyUtils.listdir('./微博'):
+        if useruid in i:
+            return useruid, author, i
+    return useruid, author, f'./微博/{author}_{useruid}'
+
+
+def getpics(page, path):
+    page = page[0]
+    # 图片元素列表
+    es = page.elements('//*[@id="app"]//main//div[contains(@class,"content")]//div[contains(@class,"woo-picture-")]/img', strict=False)
+    if es == []:
+        return
+    # 点击图片
+    page.click(es[0])
+    MyUtils.sleep(1)
+    # 查看大图
+    page.click('//*[@id="app"]//*[contains(text(),"查看大图")]')
+    for count in range(len(es)):
+        page.click('//*[@id="app"]//*[contains(@title,"查看原图")]')
+        page.switchto(-1)
+        MyUtils.pagedownload(page.url(), f'{path}/{count}', t=7)
+        page.close()
+        page.hotkey('left')
+        MyUtils.sleep(1)
+
+
+def addposters(page, useruid, author, userpath, t=2):
+    page = page[0]
+    MyUtils.sleep(t)
+    posteruids = []
+    for i in MyUtils.listdir(userpath):
+        posteruids.append(MyUtils.filename(i))
+    maxheight = -1
+    # 持续下滚
+    while maxheight < page.scrollheight():
+        maxheight = page.scrollheight()
+        MyUtils.delog(maxheight)
+        es = page.elements('//div[@class="vue-recycle-scroller__item-view"]')
+        e1s = page.elements(f'//div[@class="vue-recycle-scroller__item-view"]//a[contains(@href,"https://")]')
+        ecount=0
+        for e in es:
+            e1=e1s[ecount]
+            ecount+=1
+            href, date = e1.get_attribute('href'), MyUtils.Time(e1.get_attribute('title'))
+            posteruid = MyUtils.standarlizedFileName(f'{useruid}_{date}')
+            if posteruid in posteruids:
                 continue
-            MyUtils.setscrolltop([page, content.csv_f['y'] - 53])
-            if content.text.rfind(UserUID) < 0 and content.text.rfind(UserID) < 0 or content.text.rfind('etweet') > 0 or content.text.rfind('ollow') > 0:
-                continue
-            Name = (MyUtils.Element([content, By.XPATH, './/div[@lang]/span'], 7))
-            if not Name == None:
-                Name = MyUtils.MyName(Name.text)
-            else:
-                Name = ' '
-            if os.path.exists(f'./twitter/{UserUID}/tweets/{Name}.png'):
-                if os.path.getsize(f'./twitter/{UserUID}/tweets/{Name}.png') > 1:
-                    continue
-            MyUtils.file('wb', f'./twitter/{UserUID}/tweets/{Name}.png', content.screenshot_as_png)
-            # 保存每个tweet里的图片，123开始标序，文案作为文件名
-            i = 0
-            for pic in MyUtils.Elements([content, By.XPATH, './/img']):
-                # 跳过第一个不标
-                if i == 0:
-                    i += 1
-                    continue
-                # 处理链接
-                pic = pic.get_attribute('src')
-                pic = re.sub('name=[1-9][0-9]*x[1-9][0-9]*', 'name=large', pic)
-                pic = re.sub('name=small', 'name=large', pic)
-                if pic + '\n' in LocalPictureSpectrum:
-                    continue
-                # 下载图片
-                LocalPictureSpectrum.append(pic + '\n')
-                MyUtils.pagedownload(pic, f'./twitter/{UserUID}/Pic/{Name}No.{i}.png')
-                i += 1
-            print(f'Done-文案：{Name}')
+            posteruids.append(posteruid)
+            # 下载缩略封面
+            page.elementshot(f'{userpath}/{posteruid}_poster', e,yoffset=-53,extend=True)
+            # 打开详情页
+            page.open(href)
+            page.fullscreen(path=f'{userpath}/{posteruid}', autodown=True,scale=300)
+            # 保存图片
+            getpics(page=[page], path=f'{userpath}/{posteruid}', )
+            page.close()
+        MyUtils.sleep(2)
+        page.scroll(es[-1])
+        MyUtils.sleep(3)
+
+
+if __name__ == '__main__':
+    MyUtils.setrootpath(dname=[-1])
+    page = MyUtils.Chrome(mine=True, silent=True)
+    linklist = getlinklist([page])
+    for userlink in linklist:
+        page.get(f'{userlink}')
+        MyUtils.sleep(1)
+        useruid, author, userpath = adduser([page])
+        addposters([page], useruid, author, userpath)
