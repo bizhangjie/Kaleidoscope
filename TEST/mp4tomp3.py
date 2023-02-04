8bff044380012473348e24be3b46230945449d73
v000666 <3190103335@zju.edu.cn>
2023-02-04 17:16:12 +0800

    latest -WYJ

--- TEST/mp4tomp3.py
+++ TEST/mp4tomp3.py
@@ -1,6 +1,6 @@
 import MyUtils
 
 if __name__ == '__main__':
-    tar1=r'D:\standardizedPF\repositories\皇帝剪辑\音频碎片\天煞劫无BGM-穹悠u-BV164411L7Kf-xdJf5RAFYAxUKsTd\天煞劫无BGM-穹悠u-BV164411L7Kf-xdJf5RAFYAxUKsTd.mp4'
+    tar1=r'D:\standardizedPF\repositories\皇帝剪辑\音频碎片\劫吾所成之事不可逆也-联盟大人物-BV1VY4y1r7Ty-1V3dEMz6nlKXwzMO\劫吾所成之事不可逆也-联盟大人物-BV1VY4y1r7Ty-1V3dEMz6nlKXwzMO.mp4'
     # src=(r'C:\Users\17371\Videos\cache\coverDistanceVLOGMemories-值鹭羽-BV1be4y1k7wy-ETZbbNJwrk3JconO\sample.mp3')
     MyUtils.mp4tomp3(tar1)
